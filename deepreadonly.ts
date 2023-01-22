79ce2a465f9a01dc325b59d3fcd1e158a8f9fa23
MansurIsakov <mansurisakov.dev@gmail.com>
2023-01-22 21:24:00 +0500

    feat: deep readonly solution

---
+++ deepreadonly.ts
@@ -0,0 +1,23 @@
+type X = {
+  x: {
+    a: 1;
+    b: "hi";
+  };
+  y: "hey";
+};
+
+type Expected = {
+  readonly x: {
+    readonly a: 1;
+    readonly b: "hi";
+  };
+  readonly y: "hey";
+};
+
+type Todo = DeepReadonly<X>; // should be same as `Expected`
+
+type DeepReadonly<T> = T extends Function
+  ? T
+  : {
+      readonly [P in keyof T]: DeepReadonly<T[P]>;
+    };
