e5ae8e48d27d8b452b1e8ccfcb334d831f959b40
MansurIsakov <mansurisakov.dev@gmail.com>
2023-01-22 16:29:58 +0500

    feat: includes solution

---
+++ includes.ts
@@ -0,0 +1,13 @@
+type Equal<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
+
+type Includes<Arr extends unknown[], Val> = Arr extends [infer V, ...infer Rest]
+  ? Equal<Val, V> extends true
+    ? true
+    : Includes<Rest, Val>
+  : false;
+
+type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">; // expected to be `false`
