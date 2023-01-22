8850da083245c3bfbd7b8ad16f365ddc5a740b95
MansurIsakov <mansurisakov.dev@gmail.com>
2023-01-22 21:00:27 +0500

    feat: readonly 2 solution

---
+++ readonly2.ts
@@ -0,0 +1,19 @@
+type MyReadonly2<T extends object, U extends keyof T = keyof T> = {
+  readonly [P in U]: T[P];
+} & Omit<T, U>;
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+const todo: MyReadonly2<Todo, "title" | "description"> = {
+  title: "Hey",
+  description: "foobar",
+  completed: false,
+};
+
+todo.title = "Hello"; // Error: cannot reassign a readonly property
+todo.description = "barFoo"; // Error: cannot reassign a readonly property
+todo.completed = true; // OK
